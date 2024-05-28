5a99911b77c104c0cbb92221b5ca175d93880e6d
ArleiPfutzeJr <arleipfutze.ap@gmail.com>
2024-05-28 14:53:10 -0300

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,4 @@
+@extends('templates.templates')
+@section('content')
+<h1 class="text-center">CRUD</h1>
+@endsection
---
+++ resources/views/templates/templates.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="{{url('assets/bootstrap/css/bootstrap.min.css')}}">
+  <title>Cadastro de Livros (CRUD Laravel 9)</title>
+</head>
+<body>
+  @yield('content')
+</body>
+</html>
